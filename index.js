25dd2c91f10cdf30be3204045172d590bd1902ea
Alisha Khan <140215809+alishakhan389@users.noreply.github.com>
2024-01-27 22:32:59 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyCsrxt5h1YnMmeIK5tDkFuzfWfTrv_ppxU",
+    authDomain: "login-signup-form-e25d2.firebaseapp.com",
+    projectId: "login-signup-form-e25d2",
+    storageBucket: "login-signup-form-e25d2.appspot.com",
+    messagingSenderId: "625058873376",
+    appId: "1:625058873376:web:2ed97c9ec0a5c1a1d8bc45",
+    measurementId: "G-F1D2PT09W0"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
